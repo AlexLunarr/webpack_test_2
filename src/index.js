4de7493cbbf9ab1039fa936aabcffb74f9096c74
AlexLunarr <102823767+AlexLunarr@users.noreply.github.com>
2023-08-05 00:03:49 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import './index.html';
+import './index.scss';
+import code from './img/slider-pic5_1.png' ;
+import { mult, sum } from './modules/calc';
+
+
+
+
+const imgWrap = document.querySelector('.image');
+const img = new Image();
+img.src = code;
+img.width = 700
+imgWrap.append(img)
+
+
+// console.log(mult(3, 4));
+// console.log(sum(3, 4));
